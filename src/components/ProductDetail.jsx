83b0b75affacc6fafac11664cf06eda26b79fb30
Sowmya G S <sowmyags1066@gmail.com>
2024-09-03 20:05:48 +0530

    tailwind cofig

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,19 +1,28 @@
+// src/components/ProductDetail.js
+
 import React from "react";
 
 const ProductDetail = ({ product }) => {
   if (!product) {
-    return <div style={{ padding: "20px" }}>No item to display...</div>;
+    return (
+      <div className="p-5">
+        <p className="text-xs text-purple-500">Nothing to display...</p>
+        <p className="text-2xl font-bold text-black">
+          Select an item to display
+        </p>
+        <p className="text-base text-black">
+          Select an item from the master view to display details in the detail
+          view.
+        </p>
+      </div>
+    );
   }
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h2>{product.name}</h2>
-      <img
-        src={product.image}
-        alt={product.name}
-        style={{ width: "200px", marginBottom: "20px" }}
-      />
-      <p>
+    <div className="p-5">
+      <h2 className="text-2xl font-bold mb-4">{product.name}</h2>
+      <img src={product.image} alt={product.name} className="w-52 mb-5" />
+      <p className="font-semibold">
         <strong>Price:</strong> {product.price}
       </p>
       <p>{product.description}</p>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,27 +1,29 @@
+// src/components/ProductList.js
+
 import React from "react";
 
 const ProductList = ({ products, onSelectProduct }) => {
   return (
-    <div>
+    <div className="space-y-6 p-5">
       {products.map((product) => (
         <div
           key={product.id}
-          style={{
-            display: "flex",
-            marginBottom: "20px",
-            cursor: "pointer",
-          }}
+          className="cursor-pointer p-4 border border-gray-300 rounded-lg flex items-center"
           onClick={() => onSelectProduct(product)}
         >
+          {/* Image on the left side */}
           <img
             src={product.image}
             alt={product.name}
-            style={{ width: "100px", marginRight: "20px" }}
+            className="w-24 h-24 object-cover rounded-lg mr-4"
           />
+
+          {/* Product details on the right side */}
           <div>
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
-            <p>{product.description}</p>
+            <h3 className="text-lg font-semibold">{product.category}</h3>
+            <p className="text-xl font-bold">{product.name}</p>
+            <p className="text-sm text-gray-600">{product.description}</p>
+            <p className="text-lg font-semibold mt-2">{product.price}</p>
           </div>
         </div>
       ))}
