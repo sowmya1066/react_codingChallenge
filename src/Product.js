6aa4ef42b9171ccf1736747c8e2fb3525b096835
Sowmya G S <sowmyags1066@gmail.com>
2024-09-03 19:41:32 +0530

    product array

---
+++ src/Product.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: "$20",
+    description: "Description for Product 1",
+    image: "url_to_image1",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: "$30",
+    description: "Description for Product 2",
+    image: "url_to_image2",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: "$40",
+    description: "Description for Product 3",
+    image: "url_to_image3",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    price: "$50",
+    description: "Description for Product 4",
+    image: "url_to_image4",
+  },
+];
+
+export default products;
